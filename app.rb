751538fb0a0684687dd79274717c40e5fa793072
rubychilds <ruby.childs1@gmail.com>
2013-08-03 22:43:28 +0100

    changed ruby.app to follow on from index

--- app.rb
+++ app.rb
@@ -5,15 +5,8 @@ get '/' do
 end
 
 
-get '/off/:name/:from' do
-  message = "Fuck off, #{req.params.name}."
-  subtitle = "- #{params[from].capitalize!}"
-  dooutput(res, message, subtitle)
-  erb: secondName
-end
-
 get '/you/:name/:from' do
-  message = "Fuck you, #{req.params.name}."
+  message = "I Love you, #{params[name]}."
   subtitle = "- #{params[from].capitalize!}"
   dooutput(res, message, subtitle)
   erb: secondName
@@ -41,7 +34,7 @@ get '/everything/:from' do
 end
 
 get '/everyone/:from' do
-  message = "Everyone can go and fuck off."
+  message = "Everyone can go and love one another."
   subtitle = "- #{params[from].capitalize!}"
   dooutput(res, message, subtitle)
   erb: general
@@ -52,5 +45,61 @@ get '/donut/:name/:from' do
   subtitle = "- #{params[from].capitalize!}"
   dooutput(res, message, subtitle)
   erb: firstName
+end
 
+get '/shakespeare/:name/:from' do
+  message = "#{[name].capitalize!}, Love looks not with the eyes, but with the mind, And therefore is winged Cupid painted blind"
+  subtitle = "- #{params[from].capitalize!}"
+  dooutput(res, message, subtitle)
+  erb: firstName
+end
+
+get '/war/:name/:from' do
+  message = "Make Love, not war #{[name].capitalize!}, you are fucking thick"
+  subtitle = "- #{params[from].capitalize!}"
+  dooutput(res, message, subtitle)
+  erb: firstName
+end
+
+
+get '/pink/:name/:from' do
+  message = "Well, Love me red - #{[from].capitalize!}"
+  subtitle = "- #{params[from].capitalize!}"
+  dooutput(res, message, subtitle)
+  erb: firstName
+end
+
+get '/life/:name/:from' do
+  message = "Loving my life. - #{[name].capitalize!}"
+  subtitle = "- #{params[from].capitalize!}"
+  dooutput(res, message, subtitle)
+  erb: firstName
+end
+
+get '/slowly/:name/:from' do
+  message = "#{params[name]}, Love is composed of a single soul inhabiting two bodies."
+  subtitle = "- #{params[from].capitalize!}"
+  dooutput(res, message, subtitle)
+  erb: firstName
+end
+
+get '/:thing/:from' do
+  message = "Love #{params[thing]}"
+  subtitle = "- #{params[from].capitalize!}"
+  dooutput(res, message, subtitle)
+  erb: firstName
+end
+
+get '/thanks/:name/:from' do
+  message = "Thank you very much #{params[name].capitalize!}"
+  subtitle = "- #{params[from].capitalize!}"
+  dooutput(res, message, subtitle)
+  erb: firstName
+end
+
+get '/flying/:name/:from' do
+  message = "I don't give a rosey Love"
+  subtitle = "- #{params[from].capitalize!}"
+  dooutput(res, message, subtitle)
+  erb: firstName
 end
